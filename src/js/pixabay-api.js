92e5ce225e0d6bd1a28a731a0bce71e6c360af57
Anton Mametov <joyfulanton1107@gmail.com>
2024-04-20 17:32:48 +0300

    part 2

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,20 +1,18 @@
-const API_Key = '43487203-09c4de35c13c8e4f76d56d253'
+const API_KEY = '43487203-09c4de35c13c8e4f76d56d253'
 const URL = 'https://pixabay.com/api/'
 
 export default function getImagesFromApi(text = '') {
-    const parameters = new URLSearchParams({
-        keys: API_Key,
-        q: text,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: 'true',
-
-    });
-    
-    return fetch(`${URL}?${parameters}`).then(response => {
-        if (!response.ok) {
-            throw new Error(response.status);
-        }
-        return response.json();
-    });
+  const parameters = new URLSearchParams({
+    key: API_KEY,
+    q: text,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+  });
+  return fetch(`${URL}?${parameters}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
 }
