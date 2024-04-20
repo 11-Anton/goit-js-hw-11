98c5b7e39c1af31f3e4be7490fe9bc1720d4f001
Anton Mametov <joyfulanton1107@gmail.com>
2024-04-20 16:34:37 +0300

    part 1

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,20 @@
+const API_Key = '43487203-09c4de35c13c8e4f76d56d253'
+const URL = 'https://pixabay.com/api/'
+
+export default function getImagesFromApi(text = '') {
+    const parameters = new URLSearchParams({
+        keys: API_Key,
+        q: text,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
+
+    });
+    
+    return fetch(`${URL}?${parameters}`).then(response => {
+        if (!response.ok) {
+            throw new Error(response.status);
+        }
+        return response.json();
+    });
+}
---
+++ src/main.js
@@ -0,0 +1,46 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import getImagesFromApi from './js/pixabay-api';
+import { createMarkup } from './js/render-functions';
+
+const ref = {
+    form: document.querySelector('.js-form'),
+    gallery: document.querySelector('.js-gallery'),
+};
+
+ref.form.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(event) {
+    event.preventDefault();
+    ref.gallery.innerHTML = '<span class="loader"></span>';
+
+    const { text } = event.currentTarget.elements;
+
+    getImagesFromApi(text.value.trim())
+        .then(data => {
+            if (data.hits.length === 0) {
+                iziToast.warning({
+                    color: '#fc6e51',
+                    message:
+                        'Sorry, there are no images matching your search query. Please try again!',
+                    position: 'topCenter',
+                });
+            }
+            ref.gallery.innerHTML = createMarkup(data.hits);
+            lightbox.refresh();
+            ref.form.reset();
+        })
+        .catch(error => {
+            alert(error);
+        });
+}
+
+const lightbox = new SimpleLightbox('.card a', {
+    captionsData: 'alt',
+    captionsDelay: 250,
+    captionsPosition: 'outside',
+});
